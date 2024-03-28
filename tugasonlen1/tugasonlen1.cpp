e34b539539390ad516d7c1e881f5f468e76cc039
elgaizzaa <161436093+elgaizzaa@users.noreply.github.com>
2024-03-28 14:39:21 +0700

    menambahkan variabel global

--- tugasonlen1/tugasonlen1.cpp
+++ tugasonlen1/tugasonlen1.cpp
@@ -5,8 +5,8 @@
 using namespace std;
 //aray of integers to hold values
 int arr[20];
-int cmp_count = 0;
-int mov_count = 0;
+int cmp_count = 0; // number of comparasion
+int mov_count = 0; // number of data movements
 int n;
 
 void input() {
