4fadcfc4bc7f40a77530d01a414eaee4254e07c8
elgaizzaa <161436093+elgaizzaa@users.noreply.github.com>
2024-03-28 14:26:11 +0700

    menambahkan perulanggan while dalam prosedur input

--- tugasonlen1/tugasonlen1.cpp
+++ tugasonlen1/tugasonlen1.cpp
@@ -8,6 +8,19 @@ int arr[20];
 int cmp_count = 0;
 int mov_count = 0;
 int n;
+
+void input() {
+    while (true)
+    {
+        cout << "masukan panjang elemen array :";
+        cin >> n;
+
+        if (n <= 20)
+            break;
+        else
+            cout << "\nMaksimumpanjang aray adalah 20" << endl;
+    }
+}
 int main()
 {
     std::cout << "Hello World!\n";
